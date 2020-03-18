2df89aed0a64f8f1bb331a59d9fac2aaa4f9df01
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-03-17 21:21:07 -0400

    Create 0058_LenthOfLastWord_best.java

---
+++ 0058_LenthOfLastWord_best.java
@@ -0,0 +1,14 @@
+public int lengthOfLastWord(String s) {
+    int len=s.length(), lastLength=0;
+    
+    while(len > 0 && s.charAt(len-1)==' '){
+        len--;
+    }
+    
+    while(len > 0 && s.charAt(len-1)!=' '){
+        lastLength++;
+        len--;
+    }
+    
+    return lastLength;
+}
