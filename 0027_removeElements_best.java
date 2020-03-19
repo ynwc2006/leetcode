7a5ecd8f8207b68ebfeefd83c77ec44acd827854
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-03-19 10:15:37 -0400

    Create 0027_removeElements_best.java

---
+++ 0027_removeElements_best.java
@@ -0,0 +1,33 @@
+
+//Approach 1: Two Pointers
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
+
+
+
+
+//Approach 2: Two Pointers - when elements to remove are rare
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    int n = nums.length;
+    while (i < n) {
+        if (nums[i] == val) {
+            nums[i] = nums[n - 1];
+            // reduce array size by one
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;
+}
+
+
