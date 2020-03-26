327e8150abb3f085e67fcf0acae2ec13a16eab14
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-03-26 14:53:18 -0400

    Update 0015_3sum_best.java

--- 0015_3sum_best.java
+++ 0015_3sum_best.java
@@ -17,3 +17,38 @@ public List<List<Integer>> threeSum(int[] num) {
     }
     return res;
 }
+
+/* 
+//best code in my own words
+
+class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int lo=i+1;
+            int hi=nums.length-1;
+            while(lo<hi){
+                if(nums[lo]+nums[hi] == 0-nums[i]){
+                    ans.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
+                    while (lo<hi && nums[lo]==nums[lo+1]) {lo++;}
+                    while (lo<hi && nums[hi]==nums[hi-1]) {hi--;}
+                    lo++;
+                    hi--;
+                }
+                else if(nums[lo]+nums[hi]<0-nums[i]){
+                    lo++;
+                }
+                else hi--;
+            }
+        }
+ 
+        return ans;
+        
+    }
+}
+
+*/
