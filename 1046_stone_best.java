90b3191c5b64933402bd8ce6964ee6a5a240ef84
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-04-12 09:49:37 -0400

    Create 1046_stone_best.java

---
+++ 1046_stone_best.java
@@ -0,0 +1,11 @@
+//Time O(NlogN)
+//Space O(N)
+
+public int lastStoneWeight(int[] A) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b - a);
+        for (int a : A)
+            pq.offer(a);
+        while (pq.size() > 1)
+            pq.offer(pq.poll() - pq.poll());
+        return pq.poll();
+    }
