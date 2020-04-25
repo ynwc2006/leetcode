873de34530ef3d2ed0755756c4f786bf19e5f7aa
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-04-24 22:30:56 -0400

    Update 0046_Permutations_best.java

--- 0046_Permutations_best.java
+++ 0046_Permutations_best.java
@@ -10,7 +10,7 @@ class Solution {
     private void backtrace(List<List<Integer>> list, List<Integer> templist,int[] nums){
         if (templist.size()==nums.length) {list.add(new ArrayList(templist)); return;}
         for(int i=0;i<nums.length;i++){
-            if(templist.contains(nums[i])) continue;
+            if(templist.contains(nums[i])) continue;//can also use an array visited[] to track is visited, cause the time complexity for contains is O(n)
             templist.add(nums[i]);
             backtrace(list,templist,nums);
             templist.remove(templist.size()-1);
