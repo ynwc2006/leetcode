f4fb34809e220205bb2de67418faf7ac66b64dbd
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-04-26 11:18:57 -0400

    Update 1143_LongestCommonSubsequence_mine.java

--- 1143_LongestCommonSubsequence_mine.java
+++ 1143_LongestCommonSubsequence_mine.java
@@ -1,3 +1,4 @@
+// recursion
 class Solution {
     int[][] memo;
     public int longestCommonSubsequence(String text1, String text2) {
@@ -15,11 +16,23 @@ class Solution {
         if (memo[end1][end2]!=-1) return memo[end1][end2];
         int t=0;
         if (text1.charAt(end1)==text2.charAt(end2)) t=1;
-                //int t1=Math.max(dp(text1,text2,i+1,j),dp(text1,text2,i,j+1));
-                //memo[i][j]=Math.max(t1,t+dp(text1,text2,i+1,j+1));
         if (t==1) memo[end1][end2]=t+dp(text1,text2,end1-1,end2-1);
         else memo[end1][end2]=Math.max(dp(text1,text2,end1-1,end2),dp(text1,text2,end1,end2-1));
             
         return memo[end1][end2];
+    }
+}
+// iteration
+class Solution {
+  
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp =new int[text1.length()+1][text2.length()+1];
+        for(int i=0;i<text1.length()+1;i++) dp[i][0]=0;
+        for(int j=0;j<text2.length()+1;j++) dp[0][j]=0;
+        for(int i=1;i<=text1.length();i++)
+            for(int j=1;j<=text2.length();j++){
+                dp[i][j]=text1.charAt(i-1)==text2.charAt(j-1)?1+dp[i-1][j-1]:Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        return dp[text1.length()][text2.length()];
     }
 }
