bc36b276c930b2650104b9c95f0838c4de95d5b7
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-05-01 10:21:47 -0400

    Update 0189_rotateArray_mine.java

--- 0189_rotateArray_mine.java
+++ 0189_rotateArray_mine.java
@@ -1,3 +1,4 @@
+//solution1
 class Solution {
     public void rotate(int[] nums, int k) {
         if(k==0) return;
@@ -11,6 +12,27 @@ class Solution {
             rotate (nums,k-1);
             
         }
+        return;
+    }
+}
+
+//solution 2
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(k==0) return;
+        if(nums.length<=1) return;
+        int[] temp=new int[k];
+        int len=nums.length;
+        k=k%len;
+        for(int i=0;i<=k-1;i++){
+            temp[i]=nums[len-k+i];
+        }
+        for(int i=len-1;i>=k;i--){
+            nums[i]=nums[i-k];
+            
+        }
+        for(int i=k-1;i>=0;i--)
+            nums[i]=temp[i];
         return;
     }
 }
