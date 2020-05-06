77295bf141b3e7224a2526a4556851a80a6ebd5e
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-05-06 10:10:39 -0400

    Create 0169_majorityElement_mine.java

---
+++ 0169_majorityElement_mine.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);   
+            if (map.get(nums[i])>nums.length/2) return nums[i];
+            }
+        return 0;
+        
+    }
+}
