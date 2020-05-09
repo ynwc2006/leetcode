326b57292e2dcb1a28d9493958ddd3211f1f148b
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2020-05-09 11:34:19 -0400

    Create 0367_bets.java

---
+++ 0367_bets.java
@@ -0,0 +1,8 @@
+//newton method
+public boolean isPerfectSquare(int num) {
+        long x = num;
+        while (x * x > num) {
+            x = (x + num / x) >> 1;
+        }
+        return x * x == num;
+    }
